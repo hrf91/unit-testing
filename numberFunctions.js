e8ab4533a421198ec39f3a5e76b5f1bd1dbccb3b
Holly Fahey <hollyfahey@hotmail.co.uk>
2022-07-12 12:48:52 +0100

    Extended coverage and syntax edit

--- numberFunctions.js
+++ numberFunctions.js
@@ -1,11 +1,20 @@
 //function
 //getLargest(3, 5, 2) -> 5
 const getLargest = (a, b, c) => {
-    return Math.max(a, b, c);
+    if (a && b && c) {
+        return Math.max(a, b, c);
+    } else {
+        throw 'Error: all inputs must have a value.';
+    }
+    
 }
 
 const getSmallest = (a, b, c) => {
-    return Math.min(a, b, c);
+    if (a && b && c) {
+        return Math.min(a, b, c);
+    } else {
+        throw 'Error: all inputs must have a value.';
+    }
 }
 
 //export the function so it's available to other files
--- tests/numberFunction.test.js
+++ tests/numberFunction.test.js
@@ -1,14 +1,22 @@
-
-const getLargest = require('../numberFunctions');
-
-const { getLargest, getSmallest } = require('../numberFunctions');
+const each = require("jest-each").default;
+const {getLargest, getSmallest} = require('../numberFunctions');
 
 
 describe("getLargest", () => {
 
 
-test("getLargest is a function", () => {
+test("it is a function", () => {
     expect(typeof getLargest).toBe("function");
+})
+
+describe("It reports invalid inputs correctly", () => {
+
+    test("It throws an error if one of the three inputs is undefined", () => {
+        expect(() => {
+            getSmallest(1, 2)
+        }).toThrow('Error: all inputs must have a value.')
+        })
+    })
 })
 
 describe("It handles normal inputs successfully", () => {
@@ -16,11 +24,20 @@ describe("It handles normal inputs successfully", () => {
     each([
         [[1, 2, 3], 3],
         [[9, 8, 7], 9],
-        [[12, 26, 19], 36]
+        [[12, 36, 19], 36]
     ]).test("%s -> %s", (arr, expected) => {
         expect(getLargest(...arr)).toEqual(expected);
     })
 
+    describe("getSmallest", () => {
 
-})
+    })
+    test("it is a function", () => {
+        expect(typeof getSmallest).toBe("function");
+    })
+        describe("It handles expected input", () => {
+            expect(getSmallest(1, 2, 3)).toEqual(1);
+
+        })
+    
 })
