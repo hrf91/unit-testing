d5a0442f14207748c3de70e655a4335076dfad19
Holly Fahey <hollyfahey@hotmail.co.uk>
2022-07-12 10:44:49 +0100

    Configure Jest tests

---
+++ getLargest.js
@@ -0,0 +1,9 @@
+//function
+//getLargest(3, 5, 2) -> 5
+const getLargest = (a, b, c) => {
+    return Math.max(a, b, c);
+}
+
+
+//export the function so it's available to other files
+module.exports = getLargest;
---
+++ tests/example.test.js
@@ -0,0 +1,11 @@
+//test("Decription of the test", () => {
+    expect() 
+
+
+test("5 is 5", () => {
+    expect(5).toBe(5);
+})
+
+test("5 is 6", () => {
+    expect(5).not.toBe(6);
+})
